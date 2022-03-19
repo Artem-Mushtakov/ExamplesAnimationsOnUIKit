2decb7f134cd02c03f4044030bd9819a64be2e97
Artem Mushtakov <a.vladimirovich@internet.ru>
2022-03-19 21:47:25 +0400

    Initial Commit

---
+++ ExamplesAnimationsOnUIKit/App/SceneDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  SceneDelegate.swift
+//  ExamplesAnimationsOnUIKit
+//
+//  Created by Artem Mushtakov on 19.03.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
---
+++ ExamplesAnimationsOnUIKit/Scenes/Main/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  ExamplesAnimationsOnUIKit
+//
+//  Created by Artem Mushtakov on 19.03.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
